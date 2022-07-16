289494599aa14e831f147dd489bdfb7a3d5225f9
DmitryKhakhin <dmitrykhakhin@gmail.com>
2022-07-17 00:39:09 +0300

    added basic structure

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# Hi, Shekhnaz! :-)
+import pygame
+import sys
+
+pygame.init()
+
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 400
+
+pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+while True:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            sys.exit()
