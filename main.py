2555a84cec7540758fbb9df86f4a22b6e9e1ba93
DmitryKhakhin <dmitrykhakhin@gmail.com>
2022-07-21 01:02:42 +0300

    added background

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import sys
 
 pygame.init()
 
-WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 400
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 600
 
-pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+FPS = 60
+
+LIGHT_BLUE = (186, 228, 252)
+
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Jumping Rabbit")
+screen.fill(LIGHT_BLUE)
+
+clock = pygame.time.Clock()
+
+pygame.display.update()
 
 while True:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             sys.exit()
 
-# git test
+    clock.tick(30)
