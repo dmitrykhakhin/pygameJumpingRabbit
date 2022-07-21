ce7937c4b3573fe25b9f6f47f2ed59b0f26526d0
DmitryKhakhin <dmitrykhakhin@gmail.com>
2022-07-22 00:04:56 +0300

    added prototype for player

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ WINDOW_WIDTH = 900
 WINDOW_HEIGHT = 600
 
 FPS = 60
+clock = pygame.time.Clock()
 
 LIGHT_BLUE = (186, 228, 252)
+PURPLE = (179, 117, 218)
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Jumping Rabbit")
 screen.fill(LIGHT_BLUE)
 
-clock = pygame.time.Clock()
+surf_player = pygame.Surface((50, 80))
+surf_player.fill(PURPLE)
+rect_player = surf_player.get_rect(centerx=WINDOW_WIDTH//2, bottom=WINDOW_HEIGHT)
 
+screen.blit(surf_player, rect_player)
 pygame.display.update()
 
 while True:
